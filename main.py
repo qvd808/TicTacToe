7923ec49bcb3ab9bc04909d384282d9f33d09660
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-11-15 18:35:34 -0800

    Add mechanism to determine the winner

--- main.py
+++ main.py
@@ -9,25 +9,29 @@ pygame.display.init()
 screen = pygame.display.set_mode((800, 600))
 
 WHITE = 255, 255, 255
-BLACK = 0,0,0
-ORANGE= 255, 127, 0
+BLACK = 0, 0, 0
+ORANGE = 255, 127, 0
+
 
 def draw_board(n, w, board):
     game_obj = []
     for i in range(n):
         for j in range(n):
-            game_obj.append(pygame.draw.rect(screen, WHITE, (i * w, j * w, w ,w)))
-            pygame.draw.rect(screen, BLACK, (i * w, j * w, w ,w), 1)
-
+            game_obj.append(pygame.draw.rect(
+                screen, WHITE, (i * w, j * w, w, w)))
+            pygame.draw.rect(screen, BLACK, (i * w, j * w, w, w), 1)
 
             if board[i][j] == "X":
-                pygame.draw.line(screen, ORANGE, (i * w, j * w), (i * w + w, j * w + w), 5)
+                pygame.draw.line(screen, ORANGE, (i * w, j * w),
+                                 (i * w + w, j * w + w), 5)
                 pygame.draw.line(screen, ORANGE, (i * w + w, j * w),
-                                 (i * w , j * w + w), 5)
+                                 (i * w, j * w + w), 5)
             if board[i][j] == "O":
-                pygame.draw.circle(screen, ORANGE, (i * w + w//2, j * w + w//2), w//2, 5)
+                pygame.draw.circle(
+                    screen, ORANGE, (i * w + w//2, j * w + w//2), w//2, 5)
 
     return game_obj
+
 
 def finishedGame(board):
     n = len(board)
@@ -37,29 +41,31 @@ def finishedGame(board):
                 return False
     return True
 
+
 def checkWinner(board) -> bool:
     n = len(board)
-    result = False
-    winner = 'tie'
+    winner = None
+
     for i in range(n):
-        player = board[i][0]
-        for j in range(1, n):
-            if player != board[i][j]:
-                return (result, None)
+        if board[i][0] == board[i][1] == board[i][2]:
+            winner = board[i][0]
+
     for i in range(n):
-        player = board[0][i]
-        for j in range(1, n):
-            if player != board[j][i]:
-                return (result, None)
-    for i in range(n):
-        if board[i][i] != board[0][0]:
-            return (result, None)
-    
-    for i in range(n):
-        if board[i][n - i -1] != board[0][2]:
-            return (result, None)
-    
-    return (not result, winner)
+        if board[0][i] == board[1][i] == board[2][i]:
+            winner = board[0][i]
+
+    if board[2][2] == board[1][1] == board[0][0]:
+        winner = board[0][0]
+
+    if board[0][2] == board[1][1] == board[2][0]:
+        winner = board[0][2]
+
+    if winner != None:
+        return winner
+    else:
+        if (finishedGame(board)):
+            return 'tie'
+        return False
 
 
 def main():
@@ -67,7 +73,7 @@ def main():
     n = 3
     w = 80
     running = True
-    board = [["" for i in range(n)] for j in range(n) ]
+    board = [["" for i in range(n)] for j in range(n)]
 
     players = ['X', 'O']
     current_player = random.randint(0, 1)
@@ -75,9 +81,7 @@ def main():
     game_obj = []
     clock = pygame.time.Clock()
 
-    while running: 
-
-
+    while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -92,21 +96,41 @@ def main():
                         board[i][j] = players[current_player]
                         current_player = (current_player + 1) % len(players)
 
+        game_obj = draw_board(n, w, board)
 
-        game_obj = draw_board(n,w, board)
-        
         pygame.display.update()
 
         pygame.display.flip()
 
-        if finishedGame(board):
-            pygame.time.wait(2000)
-            running = False
+        if checkWinner(board):
 
+            if (checkWinner(board) != 'tie'):
+
+                green = (0, 255, 0)
+                blue = (0, 0, 128)
+                font = pygame.font.SysFont('Corbel', 35)
+
+                text = font.render(
+                    f'{checkWinner(board)} has win the game', True, green, blue)
+                screen.blit(text, (400, 300))
+                pygame.display.update()
+
+                pygame.time.wait(2000)
+                running = False
+            else:
+                green = (0, 255, 0)
+                blue = (0, 0, 128)
+                font = pygame.font.SysFont('Corbel', 35)
+                text = font.render(
+                    'Tie', True, green, blue)
+                screen.blit(text, (400, 300))
+
+                pygame.display.update()
+
+                pygame.time.wait(2000)
+                running = False
 
         clock.tick(30)
-
-
 
         screen.fill(BLACK)
 
