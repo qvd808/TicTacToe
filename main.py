a8a447a5963cc690a8c529d1c2bcd196de3007bb
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-11-16 05:35:25 -0800

    Implment minimax but something is wrong

--- main.py
+++ main.py
@@ -42,22 +42,22 @@ def finishedGame(board):
     return True
 
 
-def checkWinner(board) -> bool:
+def checkWinner(board):
     n = len(board)
     winner = None
 
     for i in range(n):
-        if board[i][0] == board[i][1] == board[i][2]:
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "":
             winner = board[i][0]
 
     for i in range(n):
-        if board[0][i] == board[1][i] == board[2][i]:
+        if board[0][i] == board[1][i] == board[2][i] and board[i][0] != "":
             winner = board[0][i]
 
-    if board[2][2] == board[1][1] == board[0][0]:
+    if board[2][2] == board[1][1] == board[0][0] and board[i][0] != "":
         winner = board[0][0]
 
-    if board[0][2] == board[1][1] == board[2][0]:
+    if board[0][2] == board[1][1] == board[2][0] and board[i][0] != "":
         winner = board[0][2]
 
     if winner != None:
@@ -67,7 +67,7 @@ def checkWinner(board) -> bool:
             return 'tie'
         return False
 
-def ai_move(board):
+def ai_move(board, players, ai):
     n = len(board)
     available = []
     for i in range(n):
@@ -75,7 +75,68 @@ def ai_move(board):
             if board[i][j] == "":
                 available.append((i, j))
 
-    return random.choice(available)
+    # i, j = random.choice(available)
+
+    bestScore = float('-inf')
+    bestMove = 0, 0
+
+    for move in available:
+        i, j = move
+        board[i][j] = players[ai]
+        value = minimax(board, 0, players, ai)
+        board[i][j] = ""
+        if (value > bestScore):
+            value = bestScore
+            bestMove = move
+
+    row, col = bestMove
+
+    board[row][col] = players[ai]
+
+
+def minimax(board, depth, players, player):
+
+    result = checkWinner(board)
+    if (result != False):
+        if (result == 'X'):
+            return 1
+        elif (result == 'O'):
+            return -1
+        else:
+            return 0
+
+    n = len(board)
+
+    if (player == 1):
+        maximizeValue = float('-inf')
+
+        for i in range(n):
+            for j in range(n):
+                if (board[i][j] != ""):
+                    continue
+                board[i][j] = players[player]
+                nextPlayer = (player + 1) % len(players)
+                staticEval = minimax(board, depth, players, nextPlayer)
+                maximizeValue = max(maximizeValue, staticEval)
+                board[i][j] = ""
+
+        return maximizeValue
+
+    if (player == 0):
+        minimizeValue = float('inf')
+
+        for i in range(n):
+            for j in range(n):
+                if (board[i][j] != ""):
+                    continue
+                board[i][j] = players[player]
+                nextPlayer = (player + 1) % len(players)
+                staticEval = minimax(board, depth, players, nextPlayer)
+                minimizeValue = min(minimizeValue, staticEval)
+                board[i][j] = ""
+                
+        return minimizeValue
+
 
 def main():
 
@@ -113,8 +174,7 @@ def main():
                             board[i][j] = players[current_player]
                             current_player = (current_player + 1) % len(players)
         else:
-            i, j = ai_move(board)
-            board[i][j] = players[ai]
+            ai_move(board, players, ai)
             current_player = (current_player + 1) % len(players)
 
         game_obj = draw_board(n, w, board)
@@ -157,3 +217,22 @@ def main():
 
 
 main()
+
+# n = 3
+# w = 80
+# running = True
+# board = [["" for i in range(n)] for j in range(n)]
+
+# players = ['X', 'O']
+# current_player = random.randint(0, 1)
+
+# ai = random.randint(0, 1)
+# if ai == current_player:
+#     print('AI go first')
+# else:
+#     print('Human go first')
+
+# print(board)
+# print(current_player)
+# minimax(board, 0, players, 1)
+# # ai_move(board, 0, players, 0)
